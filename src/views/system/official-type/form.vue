9b4e3a9ec4d891223e9677edfdc19d01131daa7f
easy <45551150+easy999000@users.noreply.github.com>
2026-08-05 23:12:17 +0800

    update official-type

---
+++ src/views/system/official-type/form.vue
@@ -0,0 +1,125 @@
+<script setup lang="tsx">
+import { ref, h } from "vue";
+import { FormProps } from "./utils/types";
+import { ElInput, ElMessage, ElUpload, ElButton } from "element-plus";
+import {
+  type PlusColumn,
+  type FieldValues,
+  PlusForm
+} from "plus-pro-components";
+import {
+  api,
+  type EnquiryGroupPageParam,
+  type GetEnquiryGetEnquiryGroupPageParams,
+  type EnquiryInfoDTO
+} from "@/api";
+import ItemList from "@/views/components/item-list.vue";
+import { getKeyList, deviceDetection } from "@pureadmin/utils";
+
+import MaterialSelection from "@/views/components/material-selection.vue";
+import JobSelection from "@/views/components/job-selectiony.vue";
+import { addDialog, closeDialog } from "@/components/ReDialog";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+import Delete from "~icons/ep/delete";
+
+const props = withDefaults(defineProps<FormProps>(), {
+  formInline: () => ({})
+});
+
+const ruleFormRef = ref();
+const newFormInline = ref(props.formInline);
+
+const loading = ref(true);
+
+const loadData = async () => {
+  if (newFormInline?.value?.ID) {
+    loading.value = true;
+    try {
+      ////GetOfficialTypeByID
+      const res = await api.api.get_System_GetOfficialTypeByID({
+        ID: Number(newFormInline?.value?.ID)
+      });
+      // 规范化数据类型，避免 ElSelect 接收到 Boolean
+      const data = { ...res.Data };
+      Object.assign(newFormInline.value, data);
+    } catch (error) {
+      console.error("数据加载失败:", error);
+    } finally {
+      loading.value = false;
+    }
+  }
+};
+
+// 在 setup 中直接调用，尽早触发数据请求
+loadData();
+
+const state = ref<FieldValues>({
+  status: "1",
+  name: "",
+  rate: 4,
+  progress: 100,
+  switch: true,
+  time: new Date().toString(),
+  endTime: []
+});
+// 模拟一个从后端获取下拉数据的API
+const getAccountSelect = async (): Promise<any[]> => {
+  // 这里替换为你的实际请求
+  const selectRes = await api.api.get_Account1_GetAccountSelectList();
+  var res = selectRes.Data.map(item => ({
+    label: item.Name,
+    value: item.AccountID
+  }));
+
+  return res;
+};
+// 模拟一个从后端获取下拉数据的API
+const getRoleOptions = async (): Promise<any[]> => {
+  // 这里替换为你的实际请求
+  const selectRes = await api.api.get_System_GetRoleSelect();
+  var res = selectRes.Data.map(item => ({
+    label: item.Name,
+    value: item.RoleID
+  }));
+
+  return res;
+};
+// 模拟一个从后端获取下拉数据的API
+const getDepOptions = async (): Promise<any[]> => {
+  // 这里替换为你的实际请求
+  const selectRes = await api.api.get_System_GetDepartmentSelect();
+  var res = selectRes.Data.map(item => ({
+    label: item.Name,
+    value: item.ID
+  }));
+
+  return res;
+};
+const columns: PlusColumn[] = [
+  {
+    label: "名字",
+    prop: "Name",
+    valueType: "input"
+  }
+];
+
+function getRef() {
+  // console.log({ title: "getRef", newFormInline });
+  return ruleFormRef.value;
+}
+defineExpose({ getRef });
+</script>
+
+<template>
+  <div>
+    <PlusForm
+      ref="ruleFormRef"
+      v-model="newFormInline"
+      class="w-full m-auto"
+      :columns="columns"
+      label-width="120px"
+      label-position="right"
+      :has-footer="false"
+    />
+  </div>
+</template>
---
+++ src/views/system/official-type/utils/hook.tsx
@@ -0,0 +1,166 @@
+﻿import { reactive, ref, onMounted, h, toRaw } from "vue";
+import type { PaginationProps } from "@pureadmin/table";
+
+import {
+  api,
+  type EnquiryGroupPageParam,
+  type GetLaborGetLaborInfoPageApiParams,
+  type Officialtype
+} from "@/api";
+import editForm from "../form.vue";
+import { addDialog } from "@/components/ReDialog";
+import { message } from "@/utils/message";
+import { deviceDetection } from "@pureadmin/utils";
+export function useHook() {
+  const searchForm: GetLaborGetLaborInfoPageApiParams = reactive({});
+  const pagination = reactive<PaginationProps>({
+    total: 0,
+    pageSize: 10,
+    currentPage: 1,
+    background: true
+  });
+  const loading = ref(false);
+  const dataList = ref([]);
+  const curRow = ref();
+  const formRef = ref();
+  const pageName = ref("公章类型");
+  const columns: TableColumnList = [
+    {
+      label: "名称",
+      prop: "Name"
+    },
+    {
+      label: "操作",
+      fixed: "right",
+      width: 210,
+      slot: "operation"
+    }
+  ];
+  const resetForm = formEl => {
+    if (!formEl) return;
+    formEl.resetFields();
+    onSearch();
+  };
+  function handleDelete(row) {
+    onDel(row).then(() => {
+      onSearch();
+    });
+    /// message(`您删除了角色名称为${row.name}的这条数据`, { type: "success" });
+  }
+  async function onDel(row?: EnquiryGroupPageParam) {
+    loading.value = true;
+    function chores() {
+      message(`您删除了${pageName.value}名称为${row.Name}的这条数据`, {
+        type: "success"
+      });
+      ///   onSearch(); // 刷新表格数据
+    }
+    /////DelOfficialTypeByID
+
+    const res = await api.api.post_System_DelOfficialTypeByID(toRaw(row));
+    if (res.Code === 0) {
+      chores();
+    } else {
+      message(`删除失败，${res.Message}`, { type: "error" });
+    }
+  }
+  /** 高亮当前权限选中行 */
+  function rowStyle({ row: { id } }) {
+    return {
+      cursor: "pointer",
+      background: id === curRow.value?.id ? "var(--el-fill-color-light)" : ""
+    };
+  }
+  onMounted(async () => {
+    onSearch();
+  });
+  async function onSearch() {
+    loading.value = true;
+    ////GetOfficialTypePage
+    const { Code, Data } = await api.api.get_System_GetOfficialTypePage({
+      PageSize: pagination.pageSize,
+      PageNumber: pagination.currentPage,
+      Count: pagination.total,
+      ...toRaw(searchForm)
+    });
+    if (Code === 0) {
+      dataList.value = Data.Data;
+      pagination.total = Data.Count;
+      pagination.pageSize = Data.PageSize;
+      pagination.currentPage = Data.PageNumber;
+    }
+
+    setTimeout(() => {
+      loading.value = false;
+    }, 500);
+  }
+  function handleSizeChange(val: number) {
+    pagination.pageSize = val;
+    onSearch();
+    console.log(`${val} items per page`);
+  }
+
+  function handleCurrentChange(val: number) {
+    pagination.currentPage = val;
+    onSearch();
+    console.log(`current page: ${val}`);
+  }
+
+  function handleSelectionChange(val) {
+    console.log("handleSelectionChange", val);
+  }
+
+  function openDialog(title = "新增", row?: Officialtype) {
+    addDialog({
+      title: `${title}${pageName.value}`,
+      props: {
+        formInline: {
+          ...row,
+          higherDeptOptions: []
+        }
+      },
+      width: "80%",
+      draggable: true,
+      fullscreen: deviceDetection(),
+      fullscreenIcon: true,
+      closeOnClickModal: false,
+      contentRenderer: () => h(editForm, { ref: formRef, formInline: null }),
+      beforeSure: (done, { options }) => {
+        const curData = options.props.formInline as Officialtype;
+        function chores(res2: any) {
+          if (res2.Code !== 0) {
+            message(`操作失败，${res2.Message}`, { type: "error" });
+            return;
+          }
+          message(`您${title}了任务名称为${curData.Name}的这条数据`, {
+            type: "success"
+          });
+          done(); // 关闭弹框
+          onSearch(); // 刷新表格数据
+        }
+        // 表单规则校验通过  ///////UpdateOfficialType
+        api.api.post_System_UpdateOfficialType(toRaw(curData)).then(res => {
+          // 实际开发先调用修改接口，再进行下面操作
+          chores(res);
+        });
+      }
+    });
+  }
+  return {
+    searchForm,
+    onSearch,
+    columns,
+    dataList,
+    curRow,
+    pagination,
+    resetForm,
+    rowStyle,
+    openDialog,
+    handleSizeChange,
+    handleCurrentChange,
+    handleSelectionChange,
+    handleDelete,
+    pageName,
+    loading
+  };
+}
